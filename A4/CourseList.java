3d6ec6a06047e9254a764c2c3b0774976974222d
Alain Euksuzian <alain-mokour@hotmail.com>
2024-01-30 19:15:36 -0500

    made progress

--- A4/CourseList.java
+++ A4/CourseList.java
@@ -136,4 +136,151 @@ public class CourseList {
             size++;
         }
     }
+
+    /**
+     * deletes a node either at head, tail or between 2 nodes. based on user argument location input
+     * @param index location on node to delete
+     */
+    public void deleteFromIndex(int index){
+        if (index < 0 || index > size -1){
+            throw new NoSuchElementException("index out of bounds");
+        }
+        else if (index == 0){
+            head = head.link;
+            size--;
+        }
+        else if (index == size - 1){
+            CourseNode position = head;
+            CourseNode previous = null;
+
+            for (int i = 0; i < size-1; i++){
+                previous = position;
+                position = position.link;
+            }
+            previous.link = null;
+            position = null;
+            size--;
+
+        }
+        else {
+            CourseNode position = head;
+            CourseNode previous = null;
+
+            for (int i = 0; i < index ; i++){
+                previous = position;
+                position = position.link;
+            }
+            previous.link = position.link;
+            size--;
+        }
+    }
+
+    /**
+     * deletes head node
+     */
+    public void deleteFromStart(){
+        if (head != null){
+            head = head.link;
+        }
+        else {
+            throw new NoSuchElementException("No head node");
+        }
+    }
+
+    /**
+     * finds a node at index and replaces the object in node
+     * @param course object to be added
+     * @param index location to replace
+     */
+    public void replaceAtIndex(Course course, int index){
+        if (index < 0 || index > size-1){
+            System.out.println("invalid index entered");
+            return;
+        }
+        else if (index == 0){
+            head = new CourseNode(course, head.link);
+        }
+        else {
+            CourseNode previous = null;
+            CourseNode position = head;
+    
+            for (int i  =0; i < index; i++){
+                previous = position;
+                position = position.link;
+            }
+            CourseNode toAdd = new CourseNode(course, position.link);
+            previous.link = toAdd;
+        }
+    }
+    /**
+     * loops through linked list and returns location (int) of node object that has the instance equal to argument
+     * @param courseId object instance to search for
+     * @return the index (location of node), null otherwise
+     */
+    public Integer find(String courseId){
+
+        int location = 1;
+        CourseNode position = head;
+
+        while(position != null){
+            if (position.course.getCourseId().equalsIgnoreCase(courseId)){
+                return location;
+            }
+            position = position.link;
+            location++;
+
+        }
+        return null;
+    }
+
+    /**
+     * method to return true if argument matches the nodes objects instance
+     * @param courseId String id to saerch for
+     * @return true if mtach found, false otherwise
+     */
+    public boolean contains(String courseId){
+        CourseNode position = head;
+
+        while(position != null){
+            if (position.course.getCourseId().equalsIgnoreCase(courseId)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * checks if 2 linkedList are equal for the same Course object (except for course ID which is unique)
+     * @param Object that will become another LinkedList
+     * @return true if both linkedLists are equals, false otherwise
+     */
+    public boolean equals(Object object){
+        if (object == null){
+            return false;
+        }
+        else if (!(object instanceof CourseList)){
+            return false;
+        }
+        
+        else {
+            CourseList otherLinkedList = (CourseList) object;
+            if (otherLinkedList.size != this.size){
+                return false;
+            }
+            else {
+                CourseNode thisListPosition = this.head;
+                CourseNode otherListPosition = otherLinkedList.head; 
+
+                while(thisListPosition != null){
+                    //involes the equls() overrided on Course class
+                    if (!thisListPosition.course.equals(otherListPosition.course)){
+                        return false;
+                    }
+                    thisListPosition = thisListPosition.link;
+                    otherListPosition = otherListPosition.link;
+                }
+                return true;
+            }
+        }
+    }
 }
--- A4/tester.java
+++ A4/tester.java
@@ -1,16 +1,8 @@
 package A4;
-
+import java.util.Random;
 public class tester {
    public static void main(String[] args) {
-    Course firstCourse = new Course("12345", "oop2", 3, "comp248", "math201");
-
-    Course secondCourse = firstCourse.clone("9999");
-
-    Course thirdCourse = new Course("1233", "comp352", 3, "oop2", "oop3");
-
-    System.out.println(thirdCourse.isDirectlyRelated(firstCourse));
-
+     
    }
-
-    
 }
+
